a8f8cc0e09149539631a7094f228a2f2799f6751
Julian <Van Ost>
2020-03-12 11:35:20 -0700

    initial commit

---
+++ routes/itemRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const { Item } = require('../models')
+
+// GET all items
+router.get('/items', (req, res) => Item.find()
+  .then(items => res.json(items))
+  .catch(e => console.error(e)))
+
+// POST one item
+router.post('/items', (req, res) => Item.create(req.body)
+  .then(() => res.sendStatus(200))
+  .catch(e => console.error(e)))
+
+// PUT one item
+router.put('/items/:id', (req, res) => Item.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => res.sendStatus(200))
+  .catch(e => console.error(e)))
+
+// DELETE one item
+router.delete('/items/:id', (req, res) => Item.findByIdAndDelete(req.params.id)
+  .then(() => res.sendStatus(200))
+  .catch(e => console.error(e)))
+
+module.exports = router
